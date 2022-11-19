7de1f597bcab724d8a6f102c011ea4b501d630c5
Lipatov Alexander <lipatov.work@bk.ru>
2022-11-19 19:57:23 +0300

    feat: Add migrator

---
+++ src/server/Migrator/Migrations/202211191856_MessageTable.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using FluentMigrator.Postgres;
+
+namespace Migrator.Migrations;
+
+[TimestampedMigration(2022, 11, 19, 18, 56)]
+public sealed class MessageTable : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Create.Table("Message")
+            .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+            .WithColumn("Ip").AsString(16).NotNullable()
+            .WithColumn("Text").AsString().NotNullable();
+    }
+}
---
+++ src/server/Migrator/Program.cs
@@ -0,0 +1,51 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Migrator.Migrations;
+
+var configuration = CreateConfiguration();
+var serviceProvider = CreateServices(configuration);
+
+using var scope = serviceProvider.CreateScope();
+UpdateDatabase(scope.ServiceProvider);
+
+static IConfiguration CreateConfiguration()
+{
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.Development.json", true)
+        .AddEnvironmentVariables()
+        .Build();
+
+    return config;
+}
+
+static IServiceProvider CreateServices(IConfiguration config)
+{
+    var connectionString = config.GetConnectionString("Postgres");
+
+    return new ServiceCollection()
+        .AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+            .AddPostgres()
+            .WithGlobalConnectionString(connectionString)
+            .ScanIn(typeof(MessageTable).Assembly).For.Migrations())
+        .AddLogging(lb => lb.AddFluentMigratorConsole())
+        .BuildServiceProvider(false);
+}
+
+static void UpdateDatabase(IServiceProvider serviceProvider)
+{
+    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+    var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+    if (runner.HasMigrationsToApplyUp())
+    {
+        runner.MigrateUp();
+    }
+    else
+    {
+        logger.LogInformation("No migrations were applied. The database is already up to date");
+    }
+}
